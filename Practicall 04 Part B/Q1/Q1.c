1d8f228d578e5011b813955d25ad99ab400341bb
K.A.I. Thilakarathne <135792398+Dersett@users.noreply.github.com>
2023-06-21 16:47:23 +0530

    Add files via upload

---
+++ Practicall 04 Part B/Q1/Q1.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+int main()
+{
+
+    int count=0,num=0;
+
+    //While
+    /*while(count<=100)
+    {
+        printf("%d \n",num);
+        ++num;
+        ++count;
+    }*/
+
+
+    //do while
+    /*do
+    {
+        printf("%d \n",num);
+        ++num;
+        ++count;
+    }while(count<=100);*/
+
+
+    //for
+
+    /*for(count=0;count<=100;count++){
+        printf("%d \n",num);
+        ++num;*/
+    }
+
+}
---
+++ Practicall 04 Part B/Q4.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main()
+{
+    char num;
+    printf("Enter The Numbers you want to add: ");
+    scanf("%c",num);
+    printf(num);
+
+}
